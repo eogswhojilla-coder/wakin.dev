70829d3c16b89f8d526b1777212566cc80423550
wakinnnnn_dev <eogs.whojilla@gmail.com>
2026-03-22 12:00:37 +0800

    back vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // build folder for deployment
-    rollupOptions: {
-      input: path.resolve(__dirname, 'resources/js/app.jsx'), // your main React file
-    },
-  },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
 });
